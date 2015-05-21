69874006ffbc172c75151a54213609d0b99fece8
yboker <8236138@qq.com>
2015-05-21 17:30:36 +0800

    添加基本模板

--- src/YangBo/Bundle/DreamBlogBundle/Controller/DefaultController.php
+++ src/YangBo/Bundle/DreamBlogBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace YangBo\Bundle\DreamBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction(Request $request)
     {
+        $name = $request->query->get('name');
         return $this->render('YangBoDreamBlogBundle:Default:index.html.twig', array('name' => $name));
     }
 }
