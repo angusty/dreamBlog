9239e689576c31c260387e05118bf6e852509e22
yboker <8236138@qq.com>
2015-06-15 11:54:26 +0800

    修改,使右边栏可扩展

--- src/YangBo/Bundle/DreamBlogBundle/Controller/ArticleController.php
+++ src/YangBo/Bundle/DreamBlogBundle/Controller/ArticleController.php
@@ -31,6 +31,10 @@ class ArticleController extends Controller
         );
     }
 
+    /**
+     * @deprecated 推荐使用下面的
+     * @return Response
+     */
     public function rightNewsSkeletonAction()
     {
         $em = $this->getDoctrine()->getManager();
@@ -54,4 +58,90 @@ class ArticleController extends Controller
             )
         );
     }
+
+    /**
+     * @deprecated 推荐使用下面的
+     * @return Response
+     */
+    public function rightNewsNoLinksSkeletonAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        //最新 文章 显示 条数
+        $newest_articles_show_count = $this->getParameter('newest_articles_show_count');
+        $newest_articles = $em
+            ->getRepository('YangBoDreamBlogBundle:Article')
+            ->getNewestArticles($newest_articles_show_count);
+
+        //点击排行 显示 条数
+        $most_view_articles_show_count = $this->getParameter('most_view_articles_show_count');
+        //点击排行
+        $most_view_articles = $em
+            ->getRepository('YangBoDreamBlogBundle:Article')
+            ->getMostViewArticles($most_view_articles_show_count);
+        return $this->render(
+            'YangBoDreamBlogBundle:Public:right_news_no_links_skeleton.html.twig',
+            array(
+                'newest_articles' => $newest_articles,
+                'most_view_articles' => $most_view_articles
+            )
+        );
+    }
+
+    /**
+     * 右侧最新文章
+     * @param $max
+     * @return Response
+     */
+    public function rightNewestArticlesSkeletonAction($max = '')
+    {
+        //最新 文章 显示 条数
+        $max = !empty($max) ? $max : $this->getParameter('newest_articles_show_count');
+        $em = $this->getDoctrine()->getManager();
+        $newest_articles =
+            $em
+                ->getRepository('YangBoDreamBlogBundle:Article')
+                ->getNewestArticles($max)
+            ;
+        return $this->render(
+            'YangBoDreamBlogBundle:Public:right_newest_articles_skeleton.html.twig',
+            array(
+                'newest_articles' => $newest_articles
+            )
+        );
+    }
+
+    /**
+     * 右侧最多观看次数文章
+     * @param $max
+     * @return Response
+     */
+    public function rightMostViewArticlesSkeletonAction($max = '')
+    {
+        $em = $this->getDoctrine()->getManager();
+        //点击排行 显示 条数
+        $max = !empty($max) ? $max : $this->getParameter('most_view_articles_show_count');
+        //点击排行
+        $most_view_articles = $em
+            ->getRepository('YangBoDreamBlogBundle:Article')
+            ->getMostViewArticles($max);
+        return $this->render(
+            'YangBoDreamBlogBundle:Public:right_most_view_articles_skeleton.html.twig',
+            array(
+                'most_view_articles' => $most_view_articles
+            )
+        );
+    }
+
+    /**
+     * 右侧友情链接
+     * @return Response
+     */
+    public function rightLinksSkeletonAction()
+    {
+        return $this->render(
+            'YangBoDreamBlogBundle:Public:right_links_skeleton.html.twig'
+        );
+    }
+
+
 }
