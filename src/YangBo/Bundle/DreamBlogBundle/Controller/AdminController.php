2a2e67ad721fdcaa2c75ad03b9063fae5ef99236
yboker <8236138@qq.com>
2015-06-24 17:50:55 +0800

    update

--- src/YangBo/Bundle/DreamBlogBundle/Controller/AdminController.php
+++ src/YangBo/Bundle/DreamBlogBundle/Controller/AdminController.php
@@ -1,9 +1,23 @@
 <?php
 namespace YangBo\Bundle\DreamBlogBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
+    public function indexAction()
+    {
 
+    }
+
+    public function loginAction()
+    {
+        return $this->render('YangBoDreamBlogBundle:Admin:login.html.twig');
+    }
+
+    public function testAction()
+    {
+        return new Response();
+    }
 }
