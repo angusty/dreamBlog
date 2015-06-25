fca0d49491154b21a07e3aa23fc74bf34fe9bed8
yboker <8236138@qq.com>
2015-06-25 17:32:04 +0800

    登陆

--- src/YangBo/Bundle/DreamBlogBundle/Controller/AdminController.php
+++ src/YangBo/Bundle/DreamBlogBundle/Controller/AdminController.php
@@ -2,6 +2,7 @@
 namespace YangBo\Bundle\DreamBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
@@ -11,9 +12,25 @@ class AdminController extends Controller
 
     }
 
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        return $this->render('YangBoDreamBlogBundle:Admin:login.html.twig');
+//        echo $this->getParameter('locale');
+//        echo $this->get('translator')->trans('Invalid credentials.', array(), 'security');
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $last_username = $authenticationUtils->getLastUsername();
+        $author = $request->request->all();
+        $errors = $this->get('validator')->validate($author);
+        dump($author);
+//        dump($error->getMessageKey());
+//        dump($error->getMessageData());
+        return $this->render(
+            'YangBoDreamBlogBundle:Admin:login.html.twig',
+            array(
+                'last_username' => $last_username,
+                'error' => $error
+            )
+        );
     }
 
     public function testAction()
@@ -21,3 +38,11 @@ class AdminController extends Controller
         return new Response('Admin page');
     }
 }
+
+
+
+
+
+
+
+
